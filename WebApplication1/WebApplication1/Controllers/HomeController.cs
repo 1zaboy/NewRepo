933559824606e6073c860d63af6463e939f22e02
Vlad <33203144+1zaboy@users.noreply.github.com>
2018-05-18 00:30:52 +0300

    18.05.2018 0:30 Z1

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -34,32 +34,54 @@ namespace WebApplication1.Controllers
         public ActionResult Orders()
         {
             ViewBag.Message = "Your contact page.";
-            var iid = User.Identity.GetUserId();
-            if (iid != null)
-            {
-                ViewBag.Ord = fil1.DbUserOrder.Where(t => t.User2Id != iid).ToList();
-            }
-            else
-            {
-                ViewBag.Ord = fil1.DbUserOrder;
-            }
+            ViewBag.Ord = fil1.DbUserOrder.OrderBy(t=>t.DateIn).Where(t=>t.User2Id == null).Take(30);
+            var Lis = fil1.AspNetUsers.ToList();
+            Bol = Lis.Count > 10;
+            ViewBag.Bool = Bol;
+            numb = 0;            
             return View();
+        }
+
+        public ActionResult Order_next()
+        {
+            ViewBag.Message = "Your contact page.";
+            numb += 10;
+            ViewBag.UserListOnPrint = fil1.DbUserOrder.OrderBy(t => t.DateIn).Where(t => t.User2Id == null).Skip(numb).Take(10);
+            ViewBag.Bool = Bol;
+            return View("Order");
+        }
+
+        public ActionResult Order_early()
+        {
+            ViewBag.Message = "Your contact page.";
+            numb -= 10;
+            if (numb < 0)
+                numb = 0;
+            ViewBag.UserListOnPrint = fil1.DbUserOrder.OrderBy(t => t.DateIn).Where(t => t.User2Id == null).Skip(numb).Take(10);
+            ViewBag.Bool = Bol;
+            return View("Order");
         }
         public ActionResult Developers()
         {
             ViewBag.Message = "Your contact page.";
             ViewBag.UserListOnPrint = fil1.AspNetUsers.OrderBy(t=>t.Id).Take(10);
+            var Lis = fil1.AspNetUsers.ToList();
+            Bol = Lis.Count > 10;
+            ViewBag.Bool = Bol;
             numb = 0;
             return View();
         }
   
         public dbb ff = new dbb();
+        private bool Bol = false;
         [HttpPost]
         public ActionResult Developers(AspNetUsers pic, HttpPostedFileBase uploadImage)
         {
             var r = ff.AspNetUsers.Where(t => t.UserName.Contains(pic.UserName)).ToList();
             
             ViewBag.UserListOnPrint = r;
+            
+            ViewBag.Bool = Bol;
             //return RedirectToAction("Developers");
 
             return View();
@@ -70,6 +92,7 @@ namespace WebApplication1.Controllers
             ViewBag.Message = "Your contact page.";
             numb += 10;
             ViewBag.UserListOnPrint = fil1.AspNetUsers.OrderBy(t=>t.Id).Skip(numb).Take(10);
+            ViewBag.Bool = Bol;
             return View("Developers");
         }
 
@@ -80,6 +103,7 @@ namespace WebApplication1.Controllers
             if (numb < 0)
                 numb = 0;
             ViewBag.UserListOnPrint = fil1.AspNetUsers.OrderBy(t => t.Id).Skip(numb).Take(10);
+            ViewBag.Bool = Bol;
             return View("Developers");
         }
     }
